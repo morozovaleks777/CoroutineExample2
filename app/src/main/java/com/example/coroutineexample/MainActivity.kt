c0a890aaf8d4926abcb2bd5d3bd150910627b3b8
oleksandr.o.morozov <morozovaleks@ukr.net>
2023-05-28 23:40:32 +0300

    factorial app

--- app/src/main/java/com/example/coroutineexample/MainActivity.kt
+++ app/src/main/java/com/example/coroutineexample/MainActivity.kt
@@ -1,88 +1,51 @@
 package com.example.coroutineexample
 
-import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.ProgressBar
-import android.widget.TextView
+import android.view.View
 import android.widget.Toast
-import androidx.core.view.isVisible
-import androidx.lifecycle.lifecycleScope
-import androidx.viewbinding.ViewBinding
-import by.kirich1409.viewbindingdelegate.viewBinding
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.coroutineexample.databinding.ActivityMainBinding
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity(){
-    private val viewBinding: ActivityMainBinding by viewBinding()
-val city = "Vienna"
-    val temp = "22"
-    lateinit var button : Button
-    lateinit var tv1 : TextView
-    lateinit var tv2 : TextView
-    private var isloaded = false
-    lateinit var progressBar : ProgressBar
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private val viewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        with(viewBinding){
-            this@MainActivity.button = button
-            tv1 = textView
-            tv2 = textView2
-            this@MainActivity.progressBar = progressBar
+        setContentView(binding.root)
+        observeViewModel()
+        binding.buttonCalculate.setOnClickListener {
+            viewModel.calculate(binding.editTextNumber.text.toString())
         }
-        button.setOnClickListener {
-            tv1.text = ""
-            tv2.text = ""
-            progressBar.isVisible = true
-            button.isEnabled = false
-            loadData()
+    }
+    private fun observeViewModel(){
+        viewModel.progress.observe(this){
+            if(it){
+                binding.progressBarLoading.visibility = View.VISIBLE
+                binding.buttonCalculate.isEnabled = false
+            }else{
+                binding.progressBarLoading.visibility = View.GONE
+                binding.buttonCalculate.isEnabled = true
+            }
+        }
+        viewModel.error.observe(this){
+            if (it){
+                Toast.makeText(this,"You did not entered value",Toast.LENGTH_SHORT).show()
+            }
         }
 
+        viewModel.factorial.observe(this){
+            binding.textViewFactorial.text = it
 
+        }
 
     }
-
-  fun  loadData(){
-
-   val deferredCity =   lifecycleScope.async {
-loadcity()
-       city
-      }
-   val deferredTemperature =   lifecycleScope.async {
-          loadTemperature()
-       temp
-      }
-      lifecycleScope.launch{
-      val city =   deferredCity.await()
-       val temp =    deferredTemperature.await()
-         progressBar.isVisible =false
-          button.isEnabled = true
-Toast.makeText(this@MainActivity,
-    "city: $city temperature: $temp",
-    Toast.LENGTH_SHORT).show()
-      }
-
-  }
-
-    private suspend fun loadcity():Boolean{
-        delay(3000)
-        val city = "city: $city"
-        tv1.text = city
-        isloaded = true
-        return isloaded
-    }
-    private suspend fun loadTemperature():Boolean{
-        delay(5000)
-        val temperature = "temperature: $temp \u00B0 c"
-        tv2.text = temperature
-        isloaded = true
-        return isloaded
-    }
-
-
 }
